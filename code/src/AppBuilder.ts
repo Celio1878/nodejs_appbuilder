33300740c6332308e5aa01bef5b59bca25a42a6e
Célio Vieira <celio1878@hotmail.com>
2022-04-03 02:15:51 -0300

    refactor: package engine

--- code/src/AppBuilder.ts
+++ code/src/AppBuilder.ts
@@ -2,7 +2,12 @@ import * as compression from "compression";
 import * as cors from "cors";
 import * as express from "express";
 import { NextFunction, Request, Response } from "express";
-import { AppBuilderReturnType, BuildArgs, Middleware, Module } from "./utils/ApiTypes";
+import {
+  AppBuilderReturnType,
+  BuildArgs,
+  Middleware,
+  Module,
+} from "./utils/ApiTypes";
 import { reply_error } from "./utils/reply";
 
 /**
@@ -13,45 +18,52 @@ import { reply_error } from "./utils/reply";
  */
 
 export function AppBuilder(modules: Module[] = []): AppBuilderReturnType {
-	const add_module = (module: Module) => AppBuilder(modules.concat(module));
+  const add_module = (module: Module) => AppBuilder(modules.concat(module));
 
-	return {
-		add_module,
-		build(args: Partial<BuildArgs>) {
-			const app = express();
-			app.disable("x-powered-by");
-			const default_props = default_args(args);
+  return {
+    add_module,
+    build(args: Partial<BuildArgs>) {
+      const app = express();
+      app.disable("x-powered-by");
+      const default_props = default_args(args);
 
-			const config_middlewares = [
-				express.json({ limit: "900kb" }),
-				compression(),
-				cors(default_props.cors),
-			];
+      const config_middlewares = [
+        express.json({ limit: "900kb" }),
+        compression(),
+        cors(default_props.cors),
+      ];
 
-			config_middlewares.forEach((config_middleware) => app.use("*", config_middleware));
+      config_middlewares.forEach((config_middleware) =>
+        app.use("*", config_middleware)
+      );
 
-			// adiciona middlewares ao topo da cadeia.
-			modules
-				.flatMap((module) => module.to_add_to_start_of_middleware_chain)
-				.filter((middleware) => !!middleware)
-				.forEach((middleware) => app.use("*", middleware));
+      // adiciona middlewares ao topo da cadeia.
+      modules
+        .flatMap((module) => module.to_add_to_start_of_middleware_chain)
+        .filter((middleware) => !!middleware)
+        .forEach((middleware) => app.use("*", middleware));
 
-			// adiciona endpoints.
-			const modules_map = modules.reduce(
-				(map, module) => map.set(module.name, module),
-				new Map<string, Module>()
-			);
+      // adiciona endpoints.
+      const modules_map = modules.reduce(
+        (map, module) => map.set(module.name, module),
+        new Map<string, Module>()
+      );
 
-			modules.forEach((module) =>
-				add_module_endpoints_to_app(app, module, modules_map, default_props.base_url)
-			);
+      modules.forEach((module) =>
+        add_module_endpoints_to_app(
+          app,
+          module,
+          modules_map,
+          default_props.base_url
+        )
+      );
 
-			app.use(default_props.error_handler);
+      app.use(default_props.error_handler);
 
-			return app;
-		},
-	};
-};
+      return app;
+    },
+  };
+}
 
 /**
  * Standart Args for create a API
@@ -60,31 +72,32 @@ export function AppBuilder(modules: Module[] = []): AppBuilderReturnType {
  * @returns
  */
 function default_args(args: Partial<BuildArgs>): BuildArgs {
-	const DEFAULT_CONFIG: BuildArgs = {
-		base_url: "/",
-		cors: {
-			origin: "*",
-			methods: "OPTIONS, GET, POST, PATCH, PUT, DELETE",
-			preflightContinue: false,
-			optionsSuccessStatus: 204,
-			...(args?.cors || {}),
-		},
+  const DEFAULT_CONFIG: BuildArgs = {
+    base_url: "/",
+    cors: {
+      origin: "*",
+      methods: "OPTIONS, GET, POST, PATCH, PUT, DELETE",
+      preflightContinue: false,
+      optionsSuccessStatus: 204,
+      ...(args?.cors || {}),
+    },
 
-		error_handler: (error: any, req: Request, res: Response) => reply_error(res, error),
-	};
+    error_handler: (error: any, _req: Request, res: Response) =>
+      reply_error(res, error),
+  };
 
-	const final_args: BuildArgs = {
-		...DEFAULT_CONFIG,
-		...(args || {}),
-		cors: {
-			...DEFAULT_CONFIG.cors,
-			...(args?.cors || {}),
-		},
+  const final_args: BuildArgs = {
+    ...DEFAULT_CONFIG,
+    ...(args || {}),
+    cors: {
+      ...DEFAULT_CONFIG.cors,
+      ...(args?.cors || {}),
+    },
 
-		error_handler: args?.error_handler || DEFAULT_CONFIG.error_handler,
-	};
+    error_handler: args?.error_handler || DEFAULT_CONFIG.error_handler,
+  };
 
-	return final_args;
+  return final_args;
 }
 
 /**
@@ -97,49 +110,56 @@ function default_args(args: Partial<BuildArgs>): BuildArgs {
  * @returns
  */
 function add_module_endpoints_to_app(
-	app: ReturnType<typeof express>,
-	module: Module,
-	modules_map: Map<string, Module>,
-	base_url: string = ""
+  app: ReturnType<typeof express>,
+  module: Module,
+  modules_map: Map<string, Module>,
+  base_url: string = ""
 ) {
-	(module.endpoints || []).forEach(({ method, endpoint, middlewares }) => {
-		const base_url_sanitized = remove_trim_slashes(base_url);
-		const module_base_url_sanitized = remove_trim_slashes(module.base_url || "");
-		const endpoint_sanitized = remove_trim_slashes(endpoint);
+  (module.endpoints || []).forEach(({ method, endpoint, middlewares }) => {
+    const base_url_sanitized = remove_trim_slashes(base_url);
+    const module_base_url_sanitized = remove_trim_slashes(
+      module.base_url || ""
+    );
+    const endpoint_sanitized = remove_trim_slashes(endpoint);
 
-		const final_endpoint =
-			"/" +
-			remove_trim_slashes(
-				`${base_url_sanitized}/${module_base_url_sanitized}/${endpoint_sanitized}`
-					.replace(/\/\//g, "/")
-					.replace(/\/\//g, "/")
-					.replace(/\/\//g, "/")
-			);
+    const final_endpoint =
+      "/" +
+      remove_trim_slashes(
+        `${base_url_sanitized}/${module_base_url_sanitized}/${endpoint_sanitized}`
+          .replace(/\/\//g, "/")
+          .replace(/\/\//g, "/")
+          .replace(/\/\//g, "/")
+      );
 
-		const args = [final_endpoint, middlewares.map(with_error_handling)];
+    const args = [final_endpoint, middlewares.map(with_error_handling)];
 
-		app[method.toLowerCase()].apply(app, args);
-	});
+    app[method.toLowerCase()].apply(app, args);
+  });
 
-	module.children?.forEach((child_module) =>
-		add_module_endpoints_to_app(app, child_module, modules_map, `${base_url}/${module.base_url}`)
-	);
+  module.children?.forEach((child_module) =>
+    add_module_endpoints_to_app(
+      app,
+      child_module,
+      modules_map,
+      `${base_url}/${module.base_url}`
+    )
+  );
 
-	return app;
+  return app;
 }
 
 function with_error_handling(middleware: any): Middleware {
-	return (req: Request, res: Response, next: NextFunction) => {
-		try {
-			const middleware_result = middleware(req, res, next);
+  return (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const middleware_result = middleware(req, res, next);
 
-			if (!!middleware_result && middleware_result.catch) {
-				middleware_result.catch(next);
-			}
-		} catch (error) {
-			next(error);
-		}
-	};
+      if (!!middleware_result && middleware_result.catch) {
+        middleware_result.catch(next);
+      }
+    } catch (error) {
+      next(error);
+    }
+  };
 }
 
 /**
@@ -149,19 +169,19 @@ function with_error_handling(middleware: any): Middleware {
  * @returns
  */
 function remove_trim_slashes(str: string) {
-	let result = str.trim();
+  let result = str.trim();
 
-	if (str === "/") {
-		return "";
-	}
+  if (str === "/") {
+    return "";
+  }
 
-	if (result[0] === "/") {
-		result = result.substring(1, result.length);
-	}
+  if (result[0] === "/") {
+    result = result.substring(1, result.length);
+  }
 
-	if (result[result.length - 1] === "/") {
-		result = result.substring(0, result.length - 1);
-	}
+  if (result[result.length - 1] === "/") {
+    result = result.substring(0, result.length - 1);
+  }
 
-	return result;
+  return result;
 }
--- code/src/utils/ApiTypes.ts
+++ code/src/utils/ApiTypes.ts
@@ -1,42 +1,55 @@
-import { CorsOptions } from 'cors';
-import * as express from 'express';
-import { NextFunction, Request, Response } from 'express';
+import { CorsOptions } from "cors";
+import * as express from "express";
+import { NextFunction, Request, Response } from "express";
 
 /**
  * Representa um modulo do servico.
  */
 export type Module = {
-	name: string;
-	base_url?: string;
-	endpoints?: EndpointDefinition[];
-	children?: Module[];
-	to_add_to_start_of_middleware_chain?: (Middleware | AsyncMiddleware)[];
-	public_services?: any;
+  name: string;
+  base_url?: string;
+  endpoints?: EndpointDefinition[];
+  children?: Module[];
+  to_add_to_start_of_middleware_chain: (Middleware | AsyncMiddleware)[];
+  public_services?: any;
 };
 
 export type EndpointDefinition = {
-	method: Method;
-	endpoint: string;
-	middlewares: (Middleware | AsyncMiddleware | ErrorMiddleware)[];
+  method: Method;
+  endpoint: string;
+  middlewares: (Middleware | AsyncMiddleware | ErrorMiddleware)[];
 };
 
-export type ErrorMiddleware = (error: any, req: Request, res: Response, next: NextFunction) => any;
+export type ErrorMiddleware = (
+  error: any,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => any;
 
-export type Middleware = (req: Request, res: Response, next: NextFunction) => any;
+export type Middleware = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => any;
 
-export type AsyncMiddleware = (req: Request, res: Response, next: NextFunction) => Promise<any>;
+export type AsyncMiddleware = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => Promise<any>;
 
-export type Method = 'USE' | 'GET' | 'POST' | 'PATCH' | 'PUT' | 'DELETE';
+export type Method = "USE" | "GET" | "POST" | "PATCH" | "PUT" | "DELETE";
 
 export type BuildArgs = {
-	base_url?: string;
-	cors?: CorsOptions;
-	error_handler?: ErrorMiddleware;
+  base_url?: string;
+  cors?: CorsOptions;
+  error_handler: ErrorMiddleware;
 };
 
 export type AppBuilderReturnType = {
-	add_module: (module: Module) => AppBuilderReturnType;
-	build: (args?: BuildArgs, app?: ExpressApp) => ExpressApp;
+  add_module: (module: Module) => AppBuilderReturnType;
+  build: (args: BuildArgs, app: ExpressApp) => ExpressApp;
 };
 
 export type ExpressApp = ReturnType<typeof express>;
